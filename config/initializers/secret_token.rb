0e659c7d118e8e1c44920ded6feda2e66fc13889
amyilake <ilake.chang@gmail.com>
2014-03-03 22:02:54 +0800

    strive app first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Strive::Application.config.secret_key_base = 'f6eedd932e62958fa6232ce2c27ef439fda731bf279647c5435776444b326ac4a86aca69a9485a0c2c88634efd7c69be5e4cdcac680ed0cf353ada1a96cb5581'
